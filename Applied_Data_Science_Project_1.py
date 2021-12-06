6aee94fb49363ef1664ef11944cd87167a55bfc9
samardeep331 <66426617+samardeep331@users.noreply.github.com>
2021-12-06 10:59:55 -0500

    Add files via upload

--- Applied_Data_Science_Project_1.py
+++ Applied_Data_Science_Project_1.py
@@ -688,7 +688,7 @@ def update_output2(clicks, input2):
     }
         )
 if __name__ == '__main__':
-    app.run_server(debug=True,use_reloader = False)
+    app.run_server(debug=True)
 
 
 # In[ ]:
